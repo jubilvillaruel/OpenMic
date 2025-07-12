65ca259f78a4d53eb09b10f12096885ebb504f3f
jvchiu-131 <jvchiu131@gmail.com>
2025-07-13 04:10:30 +0800

    Added login page blobs

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -1,10 +1,51 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 
 const LoginPage = () => {
   return (
-    <div className="flex min-h-screen bg-[#0a0a0a] text-white">
+    <div className=" relative min-h-screen flex bg-[#0a0a0a] text-white inset-0 -z-10 overflow-hidden">
+    
+<div>
+  <Image
+  src={"/blob.svg"}
+  alt="Blob"
+  width={700}
+  height={700}
+  className="absolute top-0 left-0 opacity-50 rounded-[60%] animate-blob2 mix-blend-screen"
+  />
+</div>
+<div>
+  <Image
+  src={"/blob2.svg"}
+  alt="Blob"
+  width={700}
+  height={700}
+  className="absolute top-0 right-0  opacity-50  rounded-[70%]  animate-blob2 "
+  />
+</div>
+
+<div>
+  <Image
+  src={"/blob3.svg"}
+  alt="Blob"
+  width={700}
+  height={700}
+  className="absolute bottom-0 left-0  opacity-50  rounded-[65%]  animate-blob3" 
+  />
+</div>
+
+
+<div>
+  <Image
+  src={"/blob4.svg"}
+  alt="Blob"
+  width={700}
+  height={700}
+  className="absolute bottom-0 right-0  opacity-50  rounded-[55%]  animate-blob4 "
+  />
+</div>
      
       <div className="flex w-full lg:w-1/2 items-center justify-center px-8 py-12">
         <div className="w-full max-w-md space-y-6">
@@ -48,9 +89,9 @@ const LoginPage = () => {
         </div>
       </div>
 
-
-       <div className="hidden lg:flex w-1/2 bg-[#0FAC7D] items-center justify-center">
-        <h1 className="text-5xl font-extrabold text-[#0a0a0a]">Welcome Back to OpenMic 🎤</h1>
+      
+      <div className="hidden lg:flex w-1/2 bg-[#0a0a0a] flex items-center justify-center">
+        <h1 className="text-5xl font-extrabold text-gray-300">Welcome Back to OpenMic 🎤</h1>
       </div>
 
     </div>
