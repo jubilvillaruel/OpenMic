bc71653108183977327e1ff34d57bc4fdff3e6e9
jvchiu-131 <jvchiu131@gmail.com>
2025-07-13 06:57:45 +0800

    changed the port from 3000 to 4000 to avoid conflict with localhost:3000 (from frontend)

--- server/dist/main.js
+++ server/dist/main.js
@@ -5,8 +5,12 @@ const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    });
     app.useGlobalPipes(new common_1.ValidationPipe());
-    await app.listen(process.env.PORT ?? 3000);
+    await app.listen(4000, '0.0.0.0');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   app.enableCors({
+    origin: 'http://localhost:3000', // Your frontend origin
+    credentials: true,               // If you're using cookies or sessions
+  });
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(4000, '0.0.0.0'); // Ensure it's not binding to ::1 only
+
 }
 bootstrap();
